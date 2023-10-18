5eba8e49b84631f44ca55717e94cfafcfb78076a
argand34 <125706829+argand34@users.noreply.github.com>
2023-10-18 15:12:59 +0900

    Delete back-end directory

--- back-end/README.md
+++
@@ -1,3 +0,0 @@
-# Back-end
-
-Now you can make the best dish with only the ingredients in your fridge.
--- back-end/middlewares/passport/index.js
+++
@@ -1,8 +0,0 @@
-import passport from "passport";
-import local from "./local.js";
-import jwt from "./jwt.js";
-
-export default () => {
-  passport.use(local);
-  passport.use(jwt);
-};
